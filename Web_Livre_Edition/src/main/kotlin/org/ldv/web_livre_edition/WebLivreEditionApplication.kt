692505e434f3067d731b015705ededb6bbca389f
Maili Guerry <maili_guerry@laposte.net>
2025-11-10 09:12:22 +0100

    projet Kotlin

---
+++ Web_Livre_Edition/src/main/kotlin/org/ldv/web_livre_edition/WebLivreEditionApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.web_livre_edition
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebLivreEditionApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebLivreEditionApplication>(*args)
+}
---
+++ Web_Livre_Edition/src/test/kotlin/org/ldv/web_livre_edition/WebLivreEditionApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ldv.web_livre_edition
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebLivreEditionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
