b52e414a439ee0a32d9c178dc688db054f107777
Maili Guerry <maili_guerry@laposte.net>
2025-11-18 10:23:36 +0100

    Suite CRUD

--- Web_Livre_Edition/src/main/kotlin/org/ldv/web_livre_edition/controller/admincontrollers/AdminLivreController.kt
+++ Web_Livre_Edition/src/main/kotlin/org/ldv/web_livre_edition/controller/admincontrollers/AdminLivreController.kt
@@ -1,4 +1,16 @@
 package org.ldv.web_livre_edition.controller.admincontrollers
 
-class AdminLivreController {
+import org.ldv.web_livre_edition.model.dao.LivreDAO
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class AdminLivreController(val livreDAO: LivreDAO) {
+    @GetMapping("/web_livre-edition/admin/livres")
+    fun index(model : Model): String{
+        var livresList = livreDAO.findAll()
+        model.addAttribute("livres",livresList)
+        return "pageAdmin/livre/indexLivre"
+    }
 }
