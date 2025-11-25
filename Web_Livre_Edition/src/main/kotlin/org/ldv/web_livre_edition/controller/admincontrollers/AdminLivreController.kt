7b671b9a827d9994a567802842a8ccfa6ecf433b
Maili Guerry <maili_guerry@laposte.net>
2025-11-25 11:58:07 +0100

    CRUD createLivre.html

--- Web_Livre_Edition/src/main/kotlin/org/ldv/web_livre_edition/controller/admincontrollers/AdminLivreController.kt
+++ Web_Livre_Edition/src/main/kotlin/org/ldv/web_livre_edition/controller/admincontrollers/AdminLivreController.kt
@@ -1,5 +1,6 @@
 package org.ldv.web_livre_edition.controller.admincontrollers
 
+import org.ldv.web_livre_edition.model.dao.GenreDAO
 import org.ldv.web_livre_edition.model.dao.LivreDAO
 import org.ldv.web_livre_edition.model.entity.Livre
 import org.springframework.stereotype.Controller
@@ -8,7 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
-class AdminLivreController(val livreDAO: LivreDAO) {
+class AdminLivreController(
+    val livreDAO: LivreDAO,
+    val genreDAO: GenreDAO
+) {
+
     @GetMapping("/web_livre_edition/admin/livres")
     fun index(model : Model): String{
         var livresList = livreDAO.findAll()
@@ -23,8 +28,12 @@ class AdminLivreController(val livreDAO: LivreDAO) {
     }
 
     @GetMapping("web_livre_edition/admin/livres/create")
-    fun create(model: Model){
-        var nouveauLivre = Livre(id = null, titre = "Lakestone", auteur = "", datePublication = "", synopsis = "",
-            image = "", prix = 10.0, genres = mutableListOf(genreRomance))
+    fun create(model: Model): String{
+        var nouveauLivre = Livre(id = null, titre = "", auteur = "", datePublication = "", synopsis = "",
+            image = "", prix = 0.0)
+        var genres = genreDAO.findAll()
+        model.addAttribute("genres", genres)
+        model.addAttribute("livre", nouveauLivre)
+        return "pageAdmin/livre/createLivre"
     }
 }
